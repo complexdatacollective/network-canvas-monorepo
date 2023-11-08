9bdbde51d873ff7cf6b9f2185445013027b6cf6d
Caden Buckhalt <halaeb@gmail.com>
2023-11-08 13:35:04 -0800

    add clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import AnalyticsView from "@/app/_components/analytics/AnalyticsView";
 import ErrorsView from "@/app/_components/errors/ErrorsView";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
     <main>
       <div className="p-12 space-y-4">
-        <h2 className="text-3xl font-bold flex flex-row">Dashboard</h2>
+        <div className="flex flex-row justify-between">
+          <h2 className="text-3xl font-bold flex flex-row">Dashboard</h2>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+
         <Tabs defaultValue="analytics" className="space-y-4">
           <TabsList>
             <TabsTrigger value="analytics">Analytics</TabsTrigger>
