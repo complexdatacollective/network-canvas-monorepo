90555161eddfb362e0a6ebc3b2a5d0242a725ab3
Turbobot <turbobot@vercel.com>
2023-11-13 13:42:55 -0800

    feat(create-turbo): apply official-starter transform

--- db/getEvents.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from "@vercel/postgres";
-import type { EventPayload as Event } from "@/@codaco/analytics";
-
-export default async function getEvents() {
-  const events = await sql`SELECT * FROM Events ;`;
-  return events.rows as Event[];
-}
