fa82315f9ad4e168542e487cb6a4fa444331a3aa
Turbobot <turbobot@vercel.com>
2023-11-13 13:42:55 -0800

    feat(create-turbo): apply official-starter transform

--- app/_components/errors/ErrorsTable/ErrorsTable.tsx
+++
@@ -1,8 +0,0 @@
-import { DataTable } from "@/components/DataTable/data-table";
-import getErrors from "@/db/getErrors";
-import { columns } from "./Columns";
-
-export default async function ErrorsTable() {
-  const errors = await getErrors();
-  return <DataTable columns={columns} data={errors} pagination={true} />;
-}
