e1f6a21da1b200564150fa60458e784ba3c3e119
Joshua Melville <joshua.melville@northwestern.edu>
2023-11-14 12:46:36 +0200

    fix monorepo structure and setup

---
+++ apps/analytics/app/page.tsx
@@ -0,0 +1,34 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import AnalyticsView from "~/app/_components/analytics/AnalyticsView";
+import ErrorsView from "~/app/_components/errors/ErrorsView";
+import { UserButton } from "@clerk/nextjs";
+import UserManagementDialog from "./_components/users/UserManagementDialog";
+
+export default function DashboardPage() {
+  return (
+    <main>
+      <div className="p-12 space-y-4">
+        <div className="flex flex-row justify-between">
+          <h2 className="text-3xl font-bold flex flex-row">Dashboard</h2>
+          <div className="flex flex-row">
+            <UserManagementDialog />
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+
+        <Tabs defaultValue="analytics" className="space-y-4">
+          <TabsList>
+            <TabsTrigger value="analytics">Analytics</TabsTrigger>
+            <TabsTrigger value="errors">Errors</TabsTrigger>
+          </TabsList>
+          <TabsContent value="analytics">
+            <AnalyticsView />
+          </TabsContent>
+          <TabsContent value="errors">
+            <ErrorsView />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </main>
+  );
+}
--- apps/web/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@codaco/analytics"],
-};
-
-module.exports = nextConfig;
