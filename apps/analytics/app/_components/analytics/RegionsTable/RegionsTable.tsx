e6f0cd7424c907a364b7aced489b15f2309f734a
Caden Buckhalt <halaeb@gmail.com>
2023-11-15 11:22:59 -0800

    countries totals table

---
+++ apps/analytics/app/_components/analytics/RegionsTable/RegionsTable.tsx
@@ -0,0 +1,8 @@
+import { DataTable } from "~/components/DataTable/data-table";
+import getRegionsTotals from "~/utils/getRegionsTotals";
+import { columns } from "./Columns";
+
+export default async function ErrorsTable() {
+  const regionsTotals = await getRegionsTotals();
+  return <DataTable columns={columns} data={regionsTotals} pagination={true} />;
+}
