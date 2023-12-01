fd87db8dbcffd36a7405e954969bf9893fec0fcf
Caden Buckhalt <halaeb@gmail.com>
2023-12-01 15:01:32 -0800

    wip mock analytics view

--- apps/analytics/components/Stats.tsx
+++ apps/analytics/components/Stats.tsx
@@ -6,13 +6,13 @@ type Stat = {
 
 const Stats: React.FC<{ stats: Stat[] }> = ({ stats }) => {
   return (
-    <div className="bg-gray-900">
+    <div className="bg-gray-700 border border-white/5">
       <div className="mx-auto max-w-7xl">
         <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
           {stats.map((stat) => (
             <div
               key={stat.name}
-              className="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8"
+              className="bg-gray-800 px-4 py-6 sm:px-6 lg:px-8"
             >
               <p className="text-sm font-medium leading-6 text-gray-400">
                 {stat.name}
