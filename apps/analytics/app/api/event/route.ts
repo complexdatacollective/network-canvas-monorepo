fa0b54dc942cc2e9ade2d94c793120c15e18d539
Caden Buckhalt <halaeb@gmail.com>
2023-12-08 14:38:36 -0800

    attempt to fix cors vercel
    following vercel guides how to enable cors exactly

--- apps/analytics/app/api/event/route.ts
+++ apps/analytics/app/api/event/route.ts
@@ -1,16 +1,6 @@
-// Import necessary modules and types
 import { NextRequest, NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 import type { DispatchableAnalyticsEvent } from "@codaco/analytics";
-
-// Set headers to allow CORS
-const headers = {
-  "Access-Control-Allow-Credentials": "true",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "OPTIONS, POST",
-  "Access-Control-Allow-Headers":
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-};
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
@@ -23,15 +13,9 @@ export async function POST(request: NextRequest) {
     const errorPayload = event.error;
     try {
       await sql`INSERT INTO Errors (code, message, details, stacktrace, timestamp, installationid, path) VALUES (${errorPayload.code}, ${errorPayload.message}, ${errorPayload.details}, ${errorPayload.stacktrace}, ${timestamp}, ${event.installationId}, ${errorPayload.path});`;
-      return new NextResponse(JSON.stringify({ errorPayload }), {
-        headers,
-        status: 200,
-      });
+      return NextResponse.json({ errorPayload }, { status: 200 });
     } catch (error) {
-      return new NextResponse(JSON.stringify({ error }), {
-        headers,
-        status: 500,
-      });
+      return NextResponse.json({ error }, { status: 500 });
     }
   }
 
@@ -46,22 +30,14 @@ export async function POST(request: NextRequest) {
       ${event.installationId},
       ${event.geolocation?.countryCode}
     );`;
-    return new NextResponse(JSON.stringify({ event }), {
-      headers,
-      status: 200,
-    });
+    return NextResponse.json({ event }, { status: 200 });
   } catch (error) {
-    return new NextResponse(JSON.stringify({ error }), {
-      headers,
-      status: 500,
-    });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
 
-// Preflight request
 export async function OPTIONS() {
   return new NextResponse(null, {
-    headers,
     status: 200,
   });
 }
