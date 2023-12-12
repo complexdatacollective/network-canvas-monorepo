40eb8aab2e5ba4e553b5ee4d50010d6963caa79e
Caden Buckhalt <halaeb@gmail.com>
2023-12-12 12:04:16 -0800

    fix formatting

--- apps/analytics/app/_components/analytics/EventsTable/EventsTable.tsx
+++ apps/analytics/app/_components/analytics/EventsTable/EventsTable.tsx
@@ -4,14 +4,20 @@ import { DataTable } from "~/components/DataTable/data-table";
 import getEvents from "~/db/getEvents";
 import { columns } from "./Columns";
 import ExportButton from "../../../../components/ExportButton";
+import { Card, CardHeader, CardContent } from "~/components/ui/card";
 
 export default async function EventsTable() {
   const events = await getEvents();
 
   return (
-    <>
-      <ExportButton data={events} filename="events.tsx" />
-      <DataTable columns={columns} data={events} pagination={true} />
-    </>
+    <Card>
+      <CardHeader>Events</CardHeader>
+      <CardContent>
+        <ExportButton data={events} filename="events.tsx" />
+        <div className="mt-4">
+          <DataTable columns={columns} data={events} pagination={true} />
+        </div>
+      </CardContent>
+    </Card>
   );
 }
