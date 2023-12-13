7dcbcb5b0ad50c72e74615573ca7db057f464e1d
Joshua Melville <joshua.melville@northwestern.edu>
2023-12-13 18:19:43 +0200

    add some initial tests

--- packages/analytics/src/index.test.mjs
+++ packages/analytics/src/index.test.mjs
@@ -1,6 +1,51 @@
-
 import { AnalyticsClient } from '../dist/index.mjs';
 
-describe('Analytics client package', () => {
-  it.todo('should be able to create a new client');
+describe('Class is able to be constructed', () => {
+  it('should be able to create a new client', () => {
+    const client = new AnalyticsClient();
+    expect(client).toBeInstanceOf(AnalyticsClient);
+  });
+});
+
+describe('Initial state', () => {
+  let client;
+  beforeEach(() => {
+    client = new AnalyticsClient();
+  });
+
+  it('should initialize without an installation ID', () => {
+    const client = new AnalyticsClient();
+    expect(client.installationId).toBeUndefined();
+  });
+
+  it('should have the correct default platform URL', () => {
+    expect(client.platformUrl).toEqual('https://analytics.networkcanvas.dev');
+  })
+})
+
+describe('Supports configuration', () => {
+  let client;
+  beforeEach(() => {
+    client = new AnalyticsClient();
+  });
+
+  it('should be able to set the platform URL', () => {
+    const url = 'https://analytics.networkcanvas.com';
+    const customClient = new AnalyticsClient({ platformUrl: url });
+    expect(customClient.platformUrl).toEqual(url);
+  });
+
+  it('should be able to set the installation ID', () => {
+    const id = '1234';
+    client.setInstallationId(id);
+    expect(client.installationId).toEqual(id);
+  });
+
+  it('Does not send events if no installation ID is set, and sends them when set', () => {
+    expect(client.dispatchQueue.paused).toEqual(true);
+
+    const id = '1234';
+    client.setInstallationId(id);
+    expect(client.dispatchQueue.paused).toEqual(false);
+  });
 });
