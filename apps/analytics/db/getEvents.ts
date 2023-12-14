0d1c4b1396ad21b349adfe675f8ff4ea748bea20
Caden Buckhalt <halaeb@gmail.com>
2023-12-14 13:09:00 -0800

    initial sort order by recency

--- apps/analytics/db/getEvents.ts
+++ apps/analytics/db/getEvents.ts
@@ -3,5 +3,10 @@ import type { Event } from "~/db/schema";
 
 export default async function getEvents() {
   const events = await sql`SELECT * FROM Events ;`;
+
+  // sort by timestamp to display events in order of most recent first
+  events.rows.sort((a, b) => {
+    return b.timestamp - a.timestamp;
+  });
   return events.rows as Event[];
 }
