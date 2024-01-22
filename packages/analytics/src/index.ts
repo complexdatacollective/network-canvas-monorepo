a81900f79515a66785d9b9d62d1f184847e2fdda
mrkarimoff <mrkarimoff99@gmail.com>
2024-01-22 17:02:57 +0500

    add DataExported to AnalyticsEvent

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -19,6 +19,7 @@ export type AnalyticsEventBase = {
 export type AnalyticsEvent = AnalyticsEventBase & {
   type:
     | "InterviewCompleted"
+    | "DataExported"
     | "InterviewStarted"
     | "ProtocolInstalled"
     | "AppSetup";
