79bcf60059bd0358a12e3c12cdf5d4bde01000c3
Joshua Melville <joshua.melville@northwestern.edu>
2024-02-02 13:51:55 +0200

    rename backend package, and reimplement testing using vitest

--- apps/analytics-web/app/api/event/route.test.ts
+++ apps/analytics-web/app/api/event/route.test.ts
@@ -1,19 +1,27 @@
+import { describe, expect, vi, it, afterEach } from "vitest";
 import { testApiHandler } from "next-test-api-route-handler";
 import insertEvent from "~/db/insertEvent";
 import * as appHandler from "./route";
+import { analyticsEvent } from "@codaco/analytics";
 
-jest.mock("~/db/insertEvent", () => async (eventData: unknown) => {
-  return { data: eventData, error: null };
+vi.mock("~/db/insertEvent", () => {
+  return {
+    default: (eventData: unknown) => ({ data: eventData, error: null }),
+  };
 });
 
 describe("/api/event", () => {
-  it("should insert event to the database", async () => {
-    const eventData = {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+  it("should insert a valid event to the database", async () => {
+    const eventData: analyticsEvent = {
       type: "AppSetup",
       metadata: {
         details: "testing details",
         path: "testing path",
       },
+      countryISOCode: "US",
       installationId: "21321546453213123",
       timestamp: new Date().toString(),
     };
@@ -25,12 +33,35 @@ describe("/api/event", () => {
           method: "POST",
           body: JSON.stringify(eventData),
         });
-        expect(insertEvent).toHaveBeenCalledWith({
-          ...eventData,
-          timestamp: new Date(eventData.timestamp),
-        });
         expect(response.status).toBe(200);
         expect(await response.json()).toEqual({ event: eventData });
+      },
+    });
+  });
+
+  it("should insert a valid error into the database", async () => {
+    const eventData: analyticsEvent = {
+      type: "Error",
+      name: "TestError",
+      message: "Test message",
+      stack: "Test stack",
+      metadata: {
+        details: "testing details",
+        path: "testing path",
+      },
+      countryISOCode: "US",
+      installationId: "21321546453213123",
+      timestamp: new Date().toString(),
+    };
+
+    await testApiHandler({
+      appHandler,
+      test: async ({ fetch }) => {
+        const response = await fetch({
+          method: "POST",
+          body: JSON.stringify(eventData),
+        });
+        expect(response.status).toBe(200);
       },
     });
   });
@@ -38,11 +69,6 @@ describe("/api/event", () => {
   it("should return 400 if event is invalid", async () => {
     const eventData = {
       type: "InvalidEvent",
-      metadata: {
-        details: "testing details",
-        path: "testing path",
-      },
-      timestamp: new Date().toString(),
     };
 
     await testApiHandler({
@@ -53,41 +79,6 @@ describe("/api/event", () => {
           body: JSON.stringify(eventData),
         });
         expect(response.status).toBe(400);
-        expect(await response.json()).toEqual({ error: "Invalid event" });
-      },
-    });
-  });
-
-  it("should return 500 if there is an error inserting the event to the database", async () => {
-    const eventData = {
-      type: "AppSetup",
-      metadata: {
-        details: "testing details",
-        path: "testing path",
-      },
-      installationId: "21321546453213123",
-      timestamp: new Date().toString(),
-    };
-
-    (insertEvent as jest.Mock).mockImplementation(async (eventData) => {
-      return { data: null, error: "Error inserting events" };
-    });
-
-    await testApiHandler({
-      appHandler,
-      test: async ({ fetch }) => {
-        const response = await fetch({
-          method: "POST",
-          body: JSON.stringify(eventData),
-        });
-        expect(insertEvent).toHaveBeenCalledWith({
-          ...eventData,
-          timestamp: new Date(eventData.timestamp),
-        });
-        expect(response.status).toBe(500);
-        expect(await response.json()).toEqual({
-          error: "Error inserting events",
-        });
       },
     });
   });
