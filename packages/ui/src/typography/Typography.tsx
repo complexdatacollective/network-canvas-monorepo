e1ab7ebcdfa5e9a10711aff2b13180201f1676c5
mrkarimoff <mrkarimoff99@gmail.com>
2024-02-22 17:18:30 +0500

    fix imports in typography components

--- packages/ui/src/typography/Typography.tsx
+++ packages/ui/src/typography/Typography.tsx
@@ -1,23 +1,21 @@
-'use client';
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cn } from "../utils/shadcn";
 
-import { cva, type VariantProps } from 'class-variance-authority';
-import React from 'react';
-import { cn } from '~/utils/shadcn';
-import { Slot } from '@radix-ui/react-slot';
-
-const typographyVariants = cva('text-foreground', {
+const typographyVariants = cva("text-foreground", {
   variants: {
     variant: {
-      h1: 'scroll-m-20 text-4xl font-semibold tracking-tight',
-      h2: 'scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0',
-      h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
-      h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
-      p: 'text-base/7 [&:not(:first-child)]:mt-6',
-      blockquote: 'mt-6 border-l-2 pl-6 italic',
-      ul: 'my-6 ml-6 list-disc [&>li]:mt-2',
+      h1: "scroll-m-20 text-4xl font-semibold tracking-tight",
+      h2: "scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0",
+      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
+      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+      p: "text-base/7 [&:not(:first-child)]:mt-6",
+      blockquote: "mt-6 border-l-2 pl-6 italic",
+      ul: "my-6 ml-6 list-disc [&>li]:mt-2",
       inlineCode:
-        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
-      lead: 'text-xl',
+        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+      lead: "text-xl",
     },
   },
 });
@@ -25,18 +23,18 @@ const typographyVariants = cva('text-foreground', {
 type VariantPropType = VariantProps<typeof typographyVariants>;
 
 const variantElementMap: Record<
-  NonNullable<VariantPropType['variant']>,
+  NonNullable<VariantPropType["variant"]>,
   string
 > = {
-  h1: 'h1',
-  h2: 'h2',
-  h3: 'h3',
-  h4: 'h4',
-  p: 'p',
-  blockquote: 'blockquote',
-  inlineCode: 'code',
-  lead: 'p',
-  ul: 'ul',
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  p: "p",
+  blockquote: "blockquote",
+  inlineCode: "code",
+  lead: "p",
+  ul: "ul",
 };
 
 export type TypographyProps = {
@@ -49,7 +47,7 @@ const Typography = React.forwardRef<HTMLElement, TypographyProps>(
   ({ className, variant, as, asChild, ...props }, ref) => {
     const Comp = asChild
       ? Slot
-      : as ?? (variant ? variantElementMap[variant] : undefined) ?? 'div';
+      : as ?? (variant ? variantElementMap[variant] : undefined) ?? "div";
     return (
       <Comp
         className={cn(typographyVariants({ variant, className }))}
@@ -57,9 +55,9 @@ const Typography = React.forwardRef<HTMLElement, TypographyProps>(
         {...props}
       />
     );
-  },
+  }
 );
 
-Typography.displayName = 'Typography';
+Typography.displayName = "Typography";
 
 export default Typography;
