bd1d9945f22c9da54517ad378513d4245ff7e89b
Joshua Melville <joshua.melville@northwestern.edu>
2024-02-27 17:47:37 +0200

    migrate eslint, tsconfig, and tailwind config to tooling

--- tooling/tailwind/base.ts
+++ tooling/tailwind/base.ts
@@ -1,15 +1,9 @@
 import type { Config } from "tailwindcss";
 
-// We want each package to be responsible for its own content.
-const config: Omit<Config, "content"> = {
+export default {
+  darkMode: ["class"],
+  content: ["src/**/*.{ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
@@ -46,26 +40,9 @@ const config: Omit<Config, "content"> = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+      borderColor: {
+        DEFAULT: "hsl(var(--border))",
       },
     },
   },
-};
-export default config;
+} satisfies Config;
