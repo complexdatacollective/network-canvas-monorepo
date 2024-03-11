8f801d472b5b7118fb0e83f4797de67147a3bee5
Joshua Melville <joshua.melville@northwestern.edu>
2024-03-11 17:09:52 +0200

    implement work in progress popoutbox

--- apps/documentation/app/[locale]/[project]/_components/customComponents/WorkInProgress.tsx
+++ apps/documentation/app/[locale]/[project]/_components/customComponents/WorkInProgress.tsx
@@ -1,15 +1,28 @@
-import TipBox from './TipBox';
+import PopoutBox from '~/components/PopoutBox';
+import { Paragraph } from '@codaco/ui';
+import Image from 'next/image';
 
 const WorkInProgress = () => {
+  // TODO: translations for this
   return (
-    <TipBox danger>
-      <p className="font-bold">WORK IN PROGRESS</p>
-      <p>
+    <PopoutBox
+      title="Work in Progress"
+      iconClassName="bg-accent"
+      icon={
+        <Image
+          src="/images/work-in-progress.svg"
+          alt="Work in progress"
+          width={22}
+          height={22}
+        />
+      }
+    >
+      <Paragraph>
         This article is currently being written, and is not yet complete. If you
         require support or assistance with this topic, please contact the
         project team directly.
-      </p>
-    </TipBox>
+      </Paragraph>
+    </PopoutBox>
   );
 };
 
