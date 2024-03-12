a5ae34d0a0495bffe1303fe25dd0b05644fde9d8
mrkarimoff <mrkarimoff99@gmail.com>
2024-03-12 22:02:35 +0500

    fix video iframe overflow issue with a custom component

---
+++ apps/documentation/app/[locale]/[project]/_components/customComponents/VideoIFrame.tsx
@@ -0,0 +1,12 @@
+const VideoIFrame = ({ src, title }: { src: string; title: string }) => {
+  return (
+    <iframe
+      title={title}
+      src={src}
+      allowFullScreen
+      className="aspect-video w-full"
+    />
+  );
+};
+
+export default VideoIFrame;
