631d6dc68a8654d75296a036f60ef02bf803f7fa
Joshua Melville <joshua.melville@northwestern.edu>
2024-03-15 13:49:46 +0200

    increase mobile font size

--- apps/documentation/components/Hero.tsx
+++ apps/documentation/components/Hero.tsx
@@ -1,22 +1,32 @@
-import { Heading, Paragraph, buttonVariants } from '@codaco/ui';
+'use client';
+
+import { Paragraph, buttonVariants } from '@codaco/ui';
 import { useTranslations } from 'next-intl';
 import { Link } from '~/navigation';
 import DocSearchComponent from './DocSearchComponent';
 import { BackgroundBlobs } from '@codaco/art';
+import { motion } from 'framer-motion';
+import FancyHeading from './FancyHeading';
 
 export function Hero() {
   const t = useTranslations();
 
   return (
     <div className="h-full overflow-hidden">
-      <div className="absolute inset-0 z-[-1] bg-gradient-to-br opacity-30">
-        <BackgroundBlobs large={2} medium={3} small={8} speedFactor={3} />
-      </div>
+      <motion.div
+        className="absolute inset-0 z-[-1] bg-gradient-to-br opacity-30"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.3 }}
+      >
+        <BackgroundBlobs large={3} medium={0} small={0} speedFactor={0.5} />
+      </motion.div>
       <div className="py-16 sm:px-2 lg:relative lg:px-0 lg:py-20">
         <div className="mx-auto grid grid-cols-1 items-center gap-x-8 gap-y-16 px-4 lg:max-w-6xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
           <div className="relative md:text-center lg:text-left">
             <div className="relative">
-              <Heading variant="h1">{t('Hero.title')}</Heading>
+              <FancyHeading variant="h1" className="text-4xl">
+                {t('Hero.title')}
+              </FancyHeading>
               <Paragraph variant="lead" className="font-normal">
                 {t('Hero.tagline')}
               </Paragraph>
