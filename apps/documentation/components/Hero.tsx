148e3fae6aa73997fe5eb4b4762502d2b02cefd9
Joshua Melville <joshua.melville@northwestern.edu>
2024-03-18 13:09:40 +0200

    add robot illustration

--- apps/documentation/components/Hero.tsx
+++ apps/documentation/components/Hero.tsx
@@ -9,30 +9,13 @@ import { motion } from 'framer-motion';
 import FancyHeading from './FancyHeading';
 import FancyParagraph from './FancyParagraph';
 import { cn } from '~/lib/utils';
+import Image from 'next/image';
 
 export function Hero() {
   const t = useTranslations();
 
   return (
     <div className="h-full overflow-hidden">
-      <motion.div
-        className="absolute inset-0 z-[-1] bg-gradient-to-br opacity-30"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.2 }}
-        transition={{
-          duration: 2,
-        }}
-      >
-        <BackgroundBlobs
-          large={2}
-          medium={1}
-          small={0}
-          // speedFactor={1}
-          // filter="blur(10rem)"
-          compositeOperation="hard-light"
-          // compositeOperation="lighten"
-        />
-      </motion.div>
       <div className="py-20">
         <div className="mx-auto grid grid-cols-1 items-center gap-x-8 gap-y-16 px-4 lg:max-w-6xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
           <div className="relative md:text-center lg:text-left">
@@ -45,22 +28,34 @@ export function Hero() {
               </FancyParagraph>
               <div className="hidden pt-8 lg:block">
                 <DocSearchComponent className="!w-full text-base" large />
+                <Link
+                  href="/desktop"
+                  className={cn(
+                    buttonVariants({ variant: 'accent' }),
+                    'w-full',
+                  )}
+                >
+                  {t('ProjectSwitcher.desktop.label')}
+                </Link>
+                <Link
+                  href="/fresco"
+                  className={cn(
+                    buttonVariants({ variant: 'secondary' }),
+                    'w-full',
+                  )}
+                >
+                  {t('ProjectSwitcher.fresco.label')}
+                </Link>
               </div>
             </div>
           </div>
           <div className="flex flex-col items-center gap-6">
-            <Link
-              href="/desktop"
-              className={cn(buttonVariants({ variant: 'accent' }), 'w-full')}
-            >
-              {t('ProjectSwitcher.desktop.label')}
-            </Link>
-            <Link
-              href="/fresco"
-              className={cn(buttonVariants({ variant: 'secondary' }), 'w-full')}
-            >
-              {t('ProjectSwitcher.fresco.label')}
-            </Link>
+            <Image
+              src="images/robot.svg"
+              width={400}
+              height={400}
+              alt="Robot"
+            />
           </div>
         </div>
       </div>
