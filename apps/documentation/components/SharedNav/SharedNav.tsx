a447f41e28b6a6c0ebb61c04e9a9abba3f4debca
Joshua Melville <joshua.melville@northwestern.edu>
2024-03-18 18:58:44 +0200

    better responsiveness

--- apps/documentation/components/SharedNav/SharedNav.tsx
+++ apps/documentation/components/SharedNav/SharedNav.tsx
@@ -8,8 +8,8 @@ export default function SharedNav() {
   return (
     <motion.nav
       className={cn(
-        'sticky top-0 z-50 mx-auto flex w-full flex-auto grow-0 items-center justify-between gap-4 bg-background/80 px-4 py-2 shadow-md backdrop-blur-sm',
-        'lg:relative lg:flex lg:bg-transparent lg:px-6 lg:py-4 lg:shadow-none lg:backdrop-blur-0',
+        'sticky top-0 z-50 mx-auto flex w-full flex-auto grow-0 items-center justify-between gap-4 border-b border-border bg-background/50 px-4 py-2 backdrop-blur-sm',
+        'lg:relative lg:flex lg:border-none lg:bg-transparent lg:px-6 lg:py-4 lg:backdrop-blur-0',
       )}
       initial={{ y: -100 }}
       animate={{ y: 0 }}
--- packages/ui/src/Divider.tsx
+++ packages/ui/src/Divider.tsx
@@ -3,7 +3,7 @@ import { cn } from './utils';
 export const Divider = ({ className }: { className?: string }) => (
   <hr
     className={cn(
-      'mx-auto w-full rounded-full border-[1.5px] border-border',
+      'mx-auto w-full rounded-full border-[1.5px] border-foreground',
       className,
     )}
   />
