ba0d935f4c62dc80e41d7b21594f44b7efdd5695
Caden Buckhalt <halaeb@gmail.com>
2024-03-29 09:00:36 -0700

    fix: get total data export card use correct data

--- apps/analytics-web/app/_components/analytics/cards/TotalDataExported.tsx
+++ apps/analytics-web/app/_components/analytics/cards/TotalDataExported.tsx
@@ -1,13 +1,13 @@
-import { getTotalInterviewsStarted } from '~/utils/getTotalInterviewsStarted';
 import { SummaryCard } from '~/components/SummaryCard';
 import { type Event } from '~/app/_actions/actions';
+import { getTotalDataExported } from '~/utils/getTotalDataExported';
 
 const TotalDataExported = ({ events }: { events: Event[] }) => {
-  const totalInterviewsStarted = getTotalInterviewsStarted(events);
+  const totalDataExported = getTotalDataExported(events);
   return (
     <SummaryCard
       title="Data Exported"
-      value={totalInterviewsStarted}
+      value={totalDataExported}
       description="Total data exported across all instances of Fresco"
     />
   );
