2aefe3952c19e59b378d2034ca82baa2b7f7e40a
mrkarimoff <mrkarimoff99@gmail.com>
2024-04-15 20:36:05 +0500

    add end-to-end type safety to the mvp

--- apps/hono-server/src/index.ts
+++ apps/hono-server/src/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 
 const app = new Hono();
 
@@ -12,5 +14,39 @@ app.get('/posts/:id', (c) => {
   c.header('X-Message', 'Hi!');
   return c.text(`You want see ${page} of ${id}`);
 });
+
+const schema = z.object({
+  id: z.string(),
+  title: z.string(),
+});
+
+type Todo = z.infer<typeof schema>;
+
+const todos: Todo[] = [
+  {
+    id: '1',
+    title: 'Buy milk',
+  },
+  {
+    id: '2',
+    title: 'Buy bread',
+  },
+];
+
+const route = app
+  .post('/todo', zValidator('form', schema), (c) => {
+    const todo = c.req.valid('form');
+    todos.push(todo);
+    return c.json({
+      message: 'created!',
+    });
+  })
+  .get((c) => {
+    return c.json({
+      todos,
+    });
+  });
+
+export type AppType = typeof route;
 
 export default app;
--- apps/next-client/app/page.tsx
+++ apps/next-client/app/page.tsx
@@ -1,11 +1,26 @@
+import { type AppType } from '@codaco/api';
+import { hc } from 'hono/client';
 import baseUrl from '~/baseUrl';
 
+const client = hc<AppType>(baseUrl);
+
 export default async function Home() {
+  const res = await client.todo.$get();
+  const data = await res.json();
+
   const homeRes = await fetch(baseUrl);
   const postsRes = await fetch(`${baseUrl}/posts/12?page=1`);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Todos</h1>
+
+      <ul>
+        {data.todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+
       {homeRes.ok ? (
         <h1 className="text-center text-4xl font-bold">
           {await homeRes.text()}
