217a92472624ac3a55e2b4958d4e7235d1413ba3
Caden Buckhalt <halaeb@gmail.com>
2024-06-26 12:52:22 -0700

    use turbo prune

--- apps/analytics-web/next.config.js
+++ apps/analytics-web/next.config.js
@@ -3,13 +3,12 @@ const config = {
   reactStrictMode: true,
 
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: [
-    "@codaco/ui",
-  ],
+  transpilePackages: ['@codaco/ui'],
 
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  output: 'standalone',
 };
 
 export default config;
