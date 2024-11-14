b523011dd38f89019b11091cb6809c0815cd2deb
Joshua Melville <joshua.melville@northwestern.edu>
2024-11-14 13:48:05 +0200

    update analytics web dependencies

--- apps/analytics-web/middleware.ts
+++ apps/analytics-web/middleware.ts
@@ -1,33 +1,24 @@
 import {
-  authMiddleware,
   clerkClient,
-  redirectToSignIn,
+  clerkMiddleware,
+  createRouteMatcher,
 } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: ['/verification'],
-  ignoredRoutes: ['/api/event'],
-  async afterAuth(auth, req, _) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
+const isPublicRoute = createRouteMatcher(['/verification(.*)']);
 
-    // handle users who aren't verified
-    if (auth.userId) {
-      const user = await clerkClient.users.getUser(auth.userId);
-      const isVerified = user?.publicMetadata?.verified;
+export default clerkMiddleware(async (auth, req) => {
+  // Restrict admin route to users with specific role
+  if (!isPublicRoute(req)) {
+    const result = await auth.protect();
+    const client = await clerkClient();
+    const user = await client.users.getUser(result.userId);
+    const isVerified = user.publicMetadata.verified;
 
-      if (!isVerified && req.nextUrl.pathname !== '/verification') {
-        return NextResponse.redirect(new URL('/verification', req.nextUrl));
-      }
+    if (!isVerified) {
+      return NextResponse.redirect(new URL('/verification', req.nextUrl));
     }
-  },
+  }
 });
 
 // all routes except static files and /api/event
