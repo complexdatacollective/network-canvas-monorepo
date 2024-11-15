813a2fb6d1f3782df8123689dba075486130a339
Joshua Melville <joshua.melville@northwestern.edu>
2024-11-15 17:24:07 +0200

    make the redirect permanent to make linkinator happy

--- apps/documentation/app/page.tsx
+++ apps/documentation/app/page.tsx
@@ -4,5 +4,5 @@ export default function RootPage() {
 	// Redirect to en for now. This is a limitation of static site generation
 	// that we need to work around by creating a client component to read the
 	// navigator.language and redirect to the appropriate locale.
-	redirect("/en");
+	redirect("/en", { permanent: true });
 }
