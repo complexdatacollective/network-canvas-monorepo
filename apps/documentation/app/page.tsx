c1be65a78d4e35f824412c29e16c690eba39b3b8
Joshua Melville <joshua.melville@northwestern.edu>
2024-11-20 13:59:56 +0200

    correct permanent redirect for root page

--- apps/documentation/app/page.tsx
+++ apps/documentation/app/page.tsx
@@ -1,8 +1,8 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function RootPage() {
 	// Redirect to en for now. This is a limitation of static site generation
 	// that we need to work around by creating a client component to read the
 	// navigator.language and redirect to the appropriate locale.
-	redirect("/en", { permanent: true });
+	permanentRedirect("/en");
 }
