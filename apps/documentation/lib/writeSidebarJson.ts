627981ea908a905121012adc041fc2845c644d92
Joshua Melville <joshua.melville@northwestern.edu>
2024-11-21 11:36:02 +0200

    fix sidebar generation

--- apps/documentation/lib/writeSidebarJson.ts
+++ apps/documentation/lib/writeSidebarJson.ts
@@ -1,8 +1,8 @@
 import "dotenv/config"; // This is essential here, because helper functions (below) use env variables, but they are not available in the Node.js environment without dotenv! This file is run directly in Node via tsc.
 import matter from "gray-matter";
-import { readFileSync, readdirSync, writeFileSync } from "node:fs";
+import { readdirSync, readFileSync, writeFileSync } from "node:fs";
 import { join } from "node:path";
-import { type Locale, type TSideBar, locales } from "~/app/types";
+import { locales, type Locale, type SidebarLocaleDefinition, type TSideBar } from "~/app/types";
 import {
 	createFolderEntry,
 	createPageEntry,
@@ -25,7 +25,7 @@ function generateSidebarData() {
 
 	// Set up initial structure for sidebar data.
 	const sidebarData: TSideBar = locales.reduce((acc, locale) => {
-		acc[locale] = {} as TSideBar[Locale];
+		acc[locale] = {} as SidebarLocaleDefinition;
 		return acc;
 	}, {} as TSideBar);
 
@@ -34,16 +34,19 @@ function generateSidebarData() {
 	for (const file of sortedFiles) {
 		if (file.isDirectory()) {
 			const metadata = getMetaDataForDirectory(join(file.path, file.name));
+			const currentLocales = Object.keys(sidebarData);
 
 			if (metadata.type === "project") {
-				for (const locale of Object.keys(sidebarData) as Locale[]) {
+				for (const l of currentLocales) {
+					const locale = l as Locale;
+
 					sidebarData[locale] = {
 						...sidebarData[locale],
 						[file.name]: createProjectEntry(file, locale, metadata),
 					};
 				}
 
-				return;
+				continue;
 			}
 
 			// If this is a folder, create a folder entry
@@ -51,39 +54,45 @@ function generateSidebarData() {
 				const nestedPath = getNestedPath(file.path);
 
 				// Insert folder entry for each locale in the nested path
-				for (const locale of Object.keys(sidebarData) as Locale[]) {
+				for (const l of currentLocales) {
+					const locale = l as Locale;
 					set(sidebarData[locale], [...nestedPath, file.name], createFolderEntry(file, locale, metadata));
 				}
 
-				return;
+				continue;
 			}
 		}
 
 		// Only process files ending in .md or .mdx
-		if (!file.name.endsWith(".md") && !file.name.endsWith(".mdx")) return;
+		if (!file.name.endsWith(".md") && !file.name.endsWith(".mdx")) {
+			continue;
+		}
 
 		// Determine locale based on file name (format is `index.en.mdx` or `index.en.md`)
 		const locale = file.name.split(".")[1] as Locale | undefined;
 
 		// If there's no locale, or the locale isn't included in the type, ignore it.
 		if (!locale || !locales.includes(locale as Locale)) {
+			// eslint-disable-next-line no-console
 			console.warn(
 				`File ${file.name} is missing a locale or has a locale not defined in Locale. Locale is ${locale}. Skipping.`,
 			);
-			return;
+			continue;
 		}
 
-		// create a key based on the filename without the locale or extension
-		// biome-ignore lint/style/noNonNullAssertion: filename is known to have a value here
+		// Create a key based on the filename without the locale or extension
+		// biome-ignore lint/style/noNonNullAssertion: structure is known
 		const key = file.name.split(".")[0]!;
 
-		const nestedPath = getNestedPath(file.parentPath);
+		const nestedPath = getNestedPath(file.path);
 
-		const markdownFile = readFileSync(join(file.parentPath, file.name), "utf-8");
+		const markdownFile = readFileSync(join(file.path, file.name), "utf-8");
 		const matterResult = matter(markdownFile);
 
 		// If file has "hidden: true" in frontmatter, skip it
-		if (matterResult.data.hidden) return;
+		if (matterResult.data.hidden) {
+			continue;
+		}
 
 		set(sidebarData[locale], [...nestedPath, key], createPageEntry(file, matterResult));
 	}
@@ -96,5 +105,7 @@ try {
 
 	writeFileSync(join(process.cwd(), "public", "sidebar.json"), JSON.stringify(sidebarData, null, 2), "utf-8");
 } catch (e) {
+	// eslint-disable-next-line no-console
 	console.log("Error writing sidebar data!", e);
+	throw e;
 }
