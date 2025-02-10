6e4b51e48da9c370c7a355499ffb17aa1ab1e202
Joshua Melville <joshua.melville@northwestern.edu>
2025-02-10 14:29:23 +0200

    lint

--- apps/documentation/lib/i18n/request.ts
+++ apps/documentation/lib/i18n/request.ts
@@ -3,13 +3,12 @@ import { getRequestConfig } from "next-intl/server";
 import { type Locale, locales } from "~/app/types";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-	const locale = await requestLocale;
+	let locale = await requestLocale;
 
 	// Ensure that the incoming locale is valid
-	if (!locale ) {
-    locale = "en";
-  }
-
+	if (!locale) {
+		locale = "en";
+	}
 
 	// Validate that the incoming `locale` string exists in the locales array
 	if (!locales.includes(locale as Locale)) {
