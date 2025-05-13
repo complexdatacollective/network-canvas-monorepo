0e66bfd94d3be7f98166c0f65d6f762e26eceb50
buckhalt <buckhalt@northwestern.edu>
2025-05-13 09:21:14 -0700

    fix schema 8 migration to remove displayVariable prop

--- packages/protocol-validation/src/migrations/migrations/8.js
+++ packages/protocol-validation/src/migrations/migrations/8.js
@@ -4,14 +4,10 @@
 
 const migration = (protocol) => {
 	// Iterate node and edge types in codebook, and remove 'displayVariable' property
-	for (const type of ["nodes", "edges"]) {
-		if (protocol.codebook[type]) {
-			for (const node of protocol.codebook[type]) {
-				if (node.displayVariable) {
-					// biome-ignore lint/performance/noDelete: performance hit acceptable, as this is a one-time operation
-					delete node.displayVariable;
-				}
-			}
+	for (const type of ["node", "edge"]) {
+		for (const entityDefinition of Object.values(protocol.codebook[type])) {
+			// biome-ignore lint/performance/noDelete: performance hit acceptable, as this is a one-time operation
+			delete entityDefinition.displayVariable;
 		}
 	}
 	return protocol;
