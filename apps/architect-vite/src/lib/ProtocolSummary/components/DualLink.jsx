59a0202c2500ca09fe36eca2ddb93b9276739fd8
Joshua Melville <joshua.melville@northwestern.edu>
2025-05-28 13:41:40 +0100

    remove react router hash link use

--- apps/architect-vite/src/lib/ProtocolSummary/components/DualLink.jsx
+++ apps/architect-vite/src/lib/ProtocolSummary/components/DualLink.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { HashLink } from 'react-router-hash-link';
+import PropTypes from "prop-types";
 
 const DualLink = ({ to, children, className }) => (
-  <>
-    <HashLink smooth to={to} data-print="no" className={className}>{children}</HashLink>
-    <a href={to} data-print="only" className={className}>{children}</a>
-  </>
+	<>
+		<a href={to} data-print="only" className={className}>
+			{children}
+		</a>
+	</>
 );
 
 DualLink.propTypes = {
-  to: PropTypes.string.isRequired,
-  className: PropTypes.string,
-  children: PropTypes.node,
+	to: PropTypes.string.isRequired,
+	className: PropTypes.string,
+	children: PropTypes.node,
 };
 
 DualLink.defaultProps = {
-  children: null,
-  className: null,
+	children: null,
+	className: null,
 };
 
 export default DualLink;
