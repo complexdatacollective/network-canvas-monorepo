f7bf30097fdd34398e60c37a42ac22d35ecce732
Joshua Melville <joshua.melville@northwestern.edu>
2025-10-01 16:53:00 +0200

    adjust property names

--- packages/protocol-validation/src/schemas/8/stages/census-stages.ts
+++ packages/protocol-validation/src/schemas/8/stages/census-stages.ts
@@ -8,6 +8,7 @@ import {
 	FormSchema,
 	NodeStageSubjectSchema,
 	oneToManyDyadCensusPromptSchema,
+	promptSchema,
 	tieStrengthCensusPromptSchema,
 } from "../common";
 import { FilterSchema } from "../filters";
@@ -140,32 +141,28 @@ export const familyTreeCensusStage = baseStageSchema
 		// The node type that is created when nodes are added
 		subject: NodeStageSubjectSchema,
 		// The edge type created when any kind of family relationship exists
-		edgeType: EdgeStageSubjectSchema,
+		edgeType: EdgeStageSubjectSchema, // family relationship
 		// Variable on edgeType used to store the type of family relationship
-		relationshipTypeVariable: z.string().generateMock(() => getEdgeVariableId(0)),
+		relationshipTypeVariable: z.string().generateMock(() => getEdgeVariableId(0)), // partner, parent, ex-partner.
 		// Biological sex variable present on node type. Optional, as this may not be collected.
 		sexVariable: z
 			.string()
 			.optional()
 			.generateMock(() => getNodeVariableId(1)),
-		step1: z.object({
-			text: z.string().generateMock(() => "how many family members"),
+		scaffoldingStep: z.object({
+			text: z.string().generateMock(() => "create a family tree"),
+			showQuickStartModal: z.boolean().generateMock(() => true),
 		}),
-		step2: z.object({
-			text: z.string().generateMock(() => "enter family members"),
+		nameGenerationStep: z.object({
+			text: z.string().generateMock(() => "name family members"),
 			form: FormSchema,
 		}),
-		step3: z
-			.object({
-				text: z.string().generateMock(() => "nominate diseases"),
-				attributes: z.array(
-					z.object({
-						id: z.string(),
-						label: z.string(),
-						variable: z.string().generateMock(() => getNodeVariableId(0)),
-					}),
-				),
-			})
+		diseaseNominationStep: z
+			.array(
+				promptSchema.extend({
+					variable: z.string().generateMock(() => getNodeVariableId(0)),
+				}),
+			)
 			.optional(),
 	})
 	.generateMock((base) => ({
