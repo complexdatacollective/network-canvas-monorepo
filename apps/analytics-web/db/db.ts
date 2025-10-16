af658bd6dc15be926cce808e471b23792631e1b6
buckhalt <buckhalt@northwestern.edu>
2025-10-16 11:46:43 -0700

    remove config

--- apps/analytics-web/db/db.ts
+++ apps/analytics-web/db/db.ts
@@ -1,9 +1,6 @@
 import { sql } from "@vercel/postgres";
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { eventsTable } from "./schema";
-
-config({ path: ".env" });
 
 // Use this object to send drizzle queries to your DB
 export const db = drizzle(sql, { schema: { eventsTable } });
