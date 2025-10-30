5a5f8698d65af1b46490af68275635fbf8be0612
buckhalt <buckhalt@northwestern.edu>
2025-10-30 13:08:32 -0700

    style fix: checkboxes

--- apps/architect-vite/src/components/sections/SociogramPrompts/PromptFieldsEdges.tsx
+++ apps/architect-vite/src/components/sections/SociogramPrompts/PromptFieldsEdges.tsx
@@ -52,62 +52,60 @@ const DisplayEdges = ({ form, entity, type }: DisplayEdgesProps) => {
 	const shouldShowNetworkFilterWarning = getEdgeFilteringWarning(edgeFilters, displayEdges || []);
 
 	return (
-		<>
-			<Section
-				title="Display Edges"
-				summary={
-					<p>
-						You can display one or more edge types on this prompt. Where two nodes are connected by multiple edge types,
-						only one of those edge types will be displayed.
-					</p>
+		<Section
+			title="Display Edges"
+			summary={
+				<p>
+					You can display one or more edge types on this prompt. Where two nodes are connected by multiple edge types,
+					only one of those edge types will be displayed.
+				</p>
+			}
+			toggleable
+			startExpanded={!!displayEdges}
+			disabled={edgesForSubject.length === 0}
+			handleToggleChange={(value: boolean) => {
+				// Disallow closing when there is a disabled edge option
+				if (!value && hasDisabledEdgeOption) {
+					return false;
 				}
-				toggleable
-				startExpanded={!!displayEdges}
-				disabled={edgesForSubject.length === 0}
-				handleToggleChange={(value: boolean) => {
-					// Disallow closing when there is a disabled edge option
-					if (!value && hasDisabledEdgeOption) {
-						return false;
-					}
 
-					if (value) {
-						return true;
-					}
-
-					// Reset edge creation
-					dispatch(change(form, "edges.display", null));
+				if (value) {
 					return true;
-				}}
-				layout="vertical"
-			>
-				<Row>
-					{shouldShowNetworkFilterWarning && (
-						<Tip type="warning">
-							<p>
-								Stage level network filtering is enabled, but one or more of the edge types you have configured to
-								display on this prompt are not currently included in the filter. This means that these edges may not be
-								displayed. Either remove the stage-level network filtering, or add these edge types to the filter to
-								resolve this issue.
-							</p>
-						</Tip>
-					)}
-					{hasDisabledEdgeOption && (
-						<Tip>
-							<p>
-								The edge type being created must always be displayed. This edge type is shown in italics below, and
-								cannot be deselected.
-							</p>
-						</Tip>
-					)}
-					<Field
-						name="edges.display"
-						component={Fields.CheckboxGroup}
-						options={displayEdgesOptions}
-						label="Display edges of the following type(s)"
-					/>
-				</Row>
-			</Section>
-		</>
+				}
+
+				// Reset edge creation
+				dispatch(change(form, "edges.display", null));
+				return true;
+			}}
+			layout="vertical"
+		>
+			<Row>
+				{shouldShowNetworkFilterWarning && (
+					<Tip type="warning">
+						<p>
+							Stage level network filtering is enabled, but one or more of the edge types you have configured to display
+							on this prompt are not currently included in the filter. This means that these edges may not be displayed.
+							Either remove the stage-level network filtering, or add these edge types to the filter to resolve this
+							issue.
+						</p>
+					</Tip>
+				)}
+				{hasDisabledEdgeOption && (
+					<Tip>
+						<p>
+							The edge type being created must always be displayed. This edge type is shown in italics below, and cannot
+							be deselected.
+						</p>
+					</Tip>
+				)}
+				<Field
+					name="edges.display"
+					component={Fields.CheckboxGroup}
+					options={displayEdgesOptions}
+					label="Display edges of the following type(s)"
+				/>
+			</Row>
+		</Section>
 	);
 };
 
