eb867ead924c59a76e623767d774d2ec5c2ff76e
buckhalt <buckhalt@northwestern.edu>
2025-11-10 14:23:27 -0600

    implement scroll to top approach to reset scroll position on route change

--- apps/architect-vite/src/components/ViewManager/views/App.tsx
+++ apps/architect-vite/src/components/ViewManager/views/App.tsx
@@ -1,10 +1,12 @@
 import DialogManager from "~/components/DialogManager";
 import Routes from "~/components/Routes";
+import ScrollToTop from "~/components/ScrollToTop";
 import ToastManager from "~/components/ToastManager";
 
 const AppView = () => {
 	return (
 		<>
+			<ScrollToTop />
 			<Routes />
 			<DialogManager />
 			<ToastManager />
