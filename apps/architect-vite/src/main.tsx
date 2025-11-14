0a6e102809f040ee91ec87179f07aef7dcde3bdf
buckhalt <buckhalt@northwestern.edu>
2025-11-14 13:45:00 -0600

    feature: add posthog error tracking

--- apps/architect-vite/src/main.tsx
+++ apps/architect-vite/src/main.tsx
@@ -1,14 +1,23 @@
+import { PostHogProvider } from "@posthog/react";
+import posthog from "posthog-js";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { AppErrorBoundary } from "./components/Errors";
 import AppView from "./components/ViewManager/views/App";
 import { store } from "./ducks/store";
 
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+	api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+	defaults: "2025-05-24",
+});
+
 // TODO: Re add StrictMode when Redux form is removed
 createRoot(document.getElementById("root") as Element).render(
-	<AppErrorBoundary>
-		<Provider store={store}>
-			<AppView />
-		</Provider>
-	</AppErrorBoundary>,
+	<PostHogProvider client={posthog}>
+		<AppErrorBoundary>
+			<Provider store={store}>
+				<AppView />
+			</Provider>
+		</AppErrorBoundary>
+	</PostHogProvider>,
 );
