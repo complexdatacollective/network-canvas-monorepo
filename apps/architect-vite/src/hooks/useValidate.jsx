eb6328cea9e4fc019c4887c9179bd616a6253837
buckhalt <buckhalt@northwestern.edu>
2025-11-18 09:19:39 -0800

    fix infinite loop

--- apps/architect-vite/src/hooks/useValidate.jsx
+++ apps/architect-vite/src/hooks/useValidate.jsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 import { getValidations } from "~/utils/validations";
 
 const useValidate = (validation) => {
-	const validate = useMemo(() => getValidations(validation), [validation]);
+	const validate = useMemo(() => getValidations(validation), []);
 
 	return validate;
 };
