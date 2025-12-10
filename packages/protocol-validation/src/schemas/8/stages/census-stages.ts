ceccae2e8523554051ec035ad815d2afe4d779f6
Ian Sieg <ian.sieg@northwestern.edu>
2025-12-10 14:50:07 -0600

    removed edge relationship to ego variables, added node relationship to ego variable, added node is ego variable

--- packages/protocol-validation/src/schemas/8/stages/census-stages.ts
+++ packages/protocol-validation/src/schemas/8/stages/census-stages.ts
@@ -144,15 +144,18 @@ export const familyTreeCensusStage = baseStageSchema
 		edgeType: EdgeStageSubjectSchema, // family relationship
 		// Variable on edgeType used to store the type of family relationship
 		relationshipTypeVariable: z.string().generateMock(() => getEdgeVariableId(0)), // partner, parent, ex-partner.
-		// Variable on edgeType used to store the source's relationship to the ego
-		sourceRelationshipToEgoVariable: z.string().generateMock(() => getEdgeVariableId(0)),
-		// Variable on edgeType used to store the target's relationship to the ego
-		targetRelationshipToEgoVariable: z.string().generateMock(() => getEdgeVariableId(0)),
+		// Variable on node type used to collect the relationship to the ego. Optional, as this may not be collected.
+		relationshipToEgoVariable: z
+			.string()
+			.optional()
+			.generateMock(() => getNodeVariableId(1)),
 		// Biological sex variable present on node type. Optional, as this may not be collected.
 		sexVariable: z
 			.string()
 			.optional()
 			.generateMock(() => getNodeVariableId(1)),
+		// Variable on node type used to differentiate the ego network node from other network nodes.
+		nodeIsEgoVariable: z.boolean().generateMock(() => false),
 		scaffoldingStep: z.object({
 			text: z.string().generateMock(() => "create a family tree"),
 			showQuickStartModal: z.boolean().generateMock(() => true),
