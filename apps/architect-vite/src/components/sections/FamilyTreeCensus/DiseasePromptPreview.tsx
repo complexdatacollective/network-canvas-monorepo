9ee08d80543e26cc9eb291b3ebde7c60bafae201
buckhalt <buckhalt@northwestern.edu>
2026-01-13 08:52:38 -0800

    add disease prompt variable to preview component

--- apps/architect-vite/src/components/sections/FamilyTreeCensus/DiseasePromptPreview.tsx
+++ apps/architect-vite/src/components/sections/FamilyTreeCensus/DiseasePromptPreview.tsx
@@ -1,9 +1,40 @@
+import { get } from "es-toolkit/compat";
+import { useSelector } from "react-redux";
+import Badge from "~/components/Badge";
+import withSubject from "~/components/enhancers/withSubject";
 import { Markdown } from "~/components/Form/Fields";
+import { getColorForType } from "~/config/variables";
+import type { RootState } from "~/ducks/modules/root";
+import { getVariablesForSubject } from "~/selectors/codebook";
 
 type DiseasePromptPreviewProps = {
 	text: string;
+	variable: string;
+	entity: string;
+	type?: string | null;
 };
 
-const DiseasePromptPreview = ({ text }: DiseasePromptPreviewProps) => <Markdown label={text} />;
+const DiseasePromptPreview = ({ text, variable, entity, type = null }: DiseasePromptPreviewProps) => {
+	const subjectVariables = useSelector((state: RootState) =>
+		getVariablesForSubject(state, { entity: entity as "node" | "edge" | "ego", type: type ?? undefined }),
+	);
+	const codebookVariable = get(subjectVariables, variable, {}) as {
+		name?: string;
+		type?: string;
+	};
 
-export default DiseasePromptPreview;
+	return (
+		<div className="m-4 flex gap-2 flex-col">
+			<Markdown label={text} className="[&>p]:m-0" />
+			<div>
+				<Badge color={getColorForType(codebookVariable.type)}>
+					<strong>{codebookVariable.type}</strong>
+					{" variable: "}
+					<strong>{codebookVariable.name}</strong>
+				</Badge>
+			</div>
+		</div>
+	);
+};
+
+export default withSubject(DiseasePromptPreview);
