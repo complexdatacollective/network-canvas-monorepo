b864c7f59c504e3aadcfc9bfb00d78cfaa00eccc
buckhalt <buckhalt@northwestern.edu>
2026-01-21 12:58:29 -0800

    fix: remove after consideration field
    was not properly registering field

--- apps/architect-vite/src/components/sections/OneToManyDyadCensus/RemoveAfterConsideration.tsx
+++ apps/architect-vite/src/components/sections/OneToManyDyadCensus/RemoveAfterConsideration.tsx
@@ -1,69 +1,38 @@
-import type { UnknownAction } from "@reduxjs/toolkit";
-import type { ComponentType } from "react";
-import { useMemo, useState } from "react";
-import { useSelector } from "react-redux";
-import { change, formValueSelector } from "redux-form";
+import { Field, FormSection } from "redux-form";
 import { Row, Section } from "~/components/EditorLayout";
 import { BooleanField } from "~/components/Form/Fields";
-import { useAppDispatch } from "~/ducks/hooks";
-import type { RootState } from "~/ducks/modules/root";
-import DetachedField from "../../DetachedField";
 
-type RemoveAfterConsiderationProps = {
-	form: string;
-};
-
-const FORM_PROPERTY = "behaviours.removeAfterConsideration";
-
-const RemoveAfterConsideration = ({ form }: RemoveAfterConsiderationProps) => {
-	const dispatch = useAppDispatch();
-	const formSelector = useMemo(() => formValueSelector(form), [form]);
-	const formValue = useSelector((state: RootState) => !!formSelector(state, FORM_PROPERTY));
-
-	const [removeAfterConsideration, setRemoveAfterConsideration] = useState(formValue);
-
-	const handleChooseRemoveAfterConsideration = () => {
-		if (removeAfterConsideration) {
-			dispatch(change("edit-stage", FORM_PROPERTY, false) as UnknownAction);
-			setRemoveAfterConsideration(false);
-			return;
+const RemoveAfterConsideration = () => (
+	<Section
+		title="Remove After Consideration"
+		summary={
+			<p>
+				This toggle determines if a node should continue to be shown in the bin after it has been the main focal node.
+				If it is set to true, the node will be removed from the pool after it has been shown in the primary position for
+				consideration.
+			</p>
 		}
-
-		dispatch(change("edit-stage", FORM_PROPERTY, true) as UnknownAction);
-		setRemoveAfterConsideration(true);
-	};
-	return (
-		<Section
-			title="Remove After Consideration"
-			summary={
-				<p>
-					This toggle determines if a node should continue to be shown in the bin after it has been the main focal node.
-					If it is set to true, the node will be removed from the pool after it has been shown in the primary position
-					for consideration.
-				</p>
-			}
-		>
-			<Row>
-				<DetachedField
-					component={BooleanField as ComponentType<Record<string, unknown>>}
-					onChange={handleChooseRemoveAfterConsideration}
-					value={removeAfterConsideration}
-					validation={{ required: true }}
+	>
+		<Row>
+			<FormSection name="behaviours">
+				<Field
+					name="removeAfterConsideration"
+					component={BooleanField}
 					options={[
 						{
 							value: true,
-							label: "Yes",
+							label: "Yes, remove after consideration",
 						},
 						{
 							value: false,
-							label: "No",
+							label: "No, keep in bin after consideration",
 						},
 					]}
 					noReset
 				/>
-			</Row>
-		</Section>
-	);
-};
+			</FormSection>
+		</Row>
+	</Section>
+);
 
 export default RemoveAfterConsideration;
