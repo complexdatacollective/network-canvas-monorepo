57818e96f7ee688f69c7eeb0e53afc874442f0f8
Joshua Melville <joshua.melville@northwestern.edu>
2026-03-24 11:29:20 +0200

    feat(architect): add NominationPromptPreview for FamilyPedigree

---
+++ apps/architect-vite/src/components/sections/FamilyPedigree/NominationPromptPreview.tsx
@@ -0,0 +1,43 @@
+import { get } from "es-toolkit/compat";
+import { useSelector } from "react-redux";
+import { formValueSelector } from "redux-form";
+import Badge from "~/components/Badge";
+import { Markdown } from "~/components/Form/Fields";
+import { getColorForType } from "~/config/variables";
+import type { RootState } from "~/ducks/store";
+import { getVariablesForSubject } from "~/selectors/codebook";
+
+type NominationPromptPreviewProps = {
+	text: string;
+	variable: string;
+	form: string;
+};
+
+const NominationPromptPreview = ({ text, variable, form }: NominationPromptPreviewProps) => {
+	const nodeType = useSelector(
+		(state: RootState) => formValueSelector(form)(state, "nodeConfig.type") as string | undefined,
+	);
+
+	const subjectVariables = useSelector((state: RootState) =>
+		getVariablesForSubject(state, { entity: "node", type: nodeType }),
+	);
+	const codebookVariable = get(subjectVariables, variable, {}) as {
+		name?: string;
+		type?: string;
+	};
+
+	return (
+		<div className="m-4 flex gap-2 flex-col">
+			<Markdown label={text} className="[&>p]:m-0" />
+			<div>
+				<Badge color={getColorForType(codebookVariable.type)}>
+					<strong>{codebookVariable.type}</strong>
+					{" variable: "}
+					<strong>{codebookVariable.name}</strong>
+				</Badge>
+			</div>
+		</div>
+	);
+};
+
+export default NominationPromptPreview;
