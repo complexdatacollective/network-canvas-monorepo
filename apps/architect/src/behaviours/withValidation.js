257343a52283d84d8a2030cf3bd9d76cc7101b75
Joshua Melville <joshua.melville@northwestern.edu>
2026-06-08 12:27:08 +0200

    knip: delete dead files in legacy apps (vite-alias shim preserved)

--- apps/architect/src/behaviours/withValidation.js
+++
@@ -1,8 +0,0 @@
-import { getValidations } from '@app/utils/validations';
-import { withPropsOnChange } from 'recompose';
-
-const withValidation = withPropsOnChange(['validation'], ({ validation }) => ({
-  validate: getValidations(validation),
-}));
-
-export default withValidation;
