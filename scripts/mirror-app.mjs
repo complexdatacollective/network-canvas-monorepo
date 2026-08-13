1e3d10166dbc311f69497460cb6a3bac955de98d
Joshua Melville <joshua@northwestern.edu>
2026-08-13 08:55:06 +0200

    chore(deps): update postcss override

--- scripts/mirror-app.mjs
+++ scripts/mirror-app.mjs
@@ -200,7 +200,7 @@ overrides:
   'effect@3.17.7': '${effectVersion}'
   fast-uri: '^3.1.4'
   find-my-way: '^9.7.0'
-  postcss: '^8.5.23'
+  postcss: '^8.5.26'
   sharp: '^0.35.3'
   valibot: '^1.4.2'
 `;
